42a7abcd85fea0d7ac8e4ee2003b7ce092ab060c
Dante Faña <dfana@dfb.com.do>
2019-03-31 06:57:03 -0400

    fix main class

--- ranchera-services/src/main/java/ado/edu/pucmm/rancheraservices/RanchServicesApplication.java
+++ ranchera-services/src/main/java/ado/edu/pucmm/rancheraservices/RanchServicesApplication.java
@@ -1,8 +1,5 @@
 package ado.edu.pucmm.rancheraservices;
 
-import ado.edu.pucmm.rancheraservices.domain.Route;
-import ado.edu.pucmm.rancheraservices.domain.Stop;
-import ado.edu.pucmm.rancheraservices.repository.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RanchServicesApplication implements CommandLineRunner {
-
-	@Autowired
-	private RouteRepository routeRepository;
 
 	@Autowired
 	public static void main(String[] args) {
